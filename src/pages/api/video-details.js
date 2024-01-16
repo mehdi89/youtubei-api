e2c53be96fed99cf7722e0e39e51ff2ee810f4d7
wddyousuf <saikatyousuf@gmail.com>
2024-01-16 12:50:09 +0600

    video details api

---
+++ src/pages/api/video-details.js
@@ -0,0 +1,39 @@
+import { Client } from "youtubei";
+export default async function handler(req, res) {
+  if (req.method === "POST") {
+    const youtube = new Client();
+    const { id } = req.body;
+    const apiKey = req.headers['api-key'];
+    if (apiKey!='S#D$FG%^$#DEF%G^*$%R^T&Y*U') {
+        res.status(401).json({message:'Please provide correct api key'});
+    }
+    var video = await youtube.getVideo(id);
+    console.log(video?.channel);
+    let response={
+      'id':video?.id,
+      'channel':{
+        'youtube_channel_id':video?.channel?.id,
+        'name':video?.channel?.name,
+        'subscriberCount':video?.channel?.subscriberCount,
+        'thumbnails':video?.channel?.thumbnails,
+        'videoCount':video?.channel?.videoCount,
+        'url':video?.channel?.url,
+      },
+      'title':video?.title,
+      'chapters':video?.chapters,
+    //   'comments':video?.comments,
+      'description':video?.description,
+      'duration':video?.duration,
+      'likeCount':video?.likeCount,
+      'isLiveContent':video?.isLiveContent,
+      'uploadDate':video?.uploadDate,
+      'viewCount':video?.viewCount,
+      'transcript':video?.getTranscript(id),
+    }
+    // console.log(response);
+
+    res.status(200).json(response);
+  }else{
+    res.status(405).end();
+  }
+}
