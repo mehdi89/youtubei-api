b9bb895ce3f60b0df7641ad939a3a796005db39f
Md. Yousuf Hossain <saikatyousuf@gmail.com>
2024-03-27 13:09:59 +0600

    playlist

--- src/pages/api/playlist.js
+++ src/pages/api/playlist.js
@@ -21,24 +21,29 @@ export default async function handler(req, res) {
       }
     }
 
-    if (page > 1 && newVideos.length > 0) {
+    if (page > 1) {
+        if (newVideos.length > 0) {
+            items = newVideos?.map(function (item) {
+                return {
+                  id: item?.id,
+                  title: item?.title,
+                  duration: item?.duration,
+                  description: item?.description,
+                  isLive: item?.isLive,
+                  viewCount: item?.viewCount,
+                  uploadDate: item?.uploadDate,
+                  thumbnail: `https://img.youtube.com/vi/${item?.id}/mqdefault.jpg`,
+                  channelName: item?.channel.name,
+                  channelID: item?.channel.id,
+                };
+              });
+        }else{
+            items=[];
+        }
     //   if (page == 2) {
     //     newVideos = newVideos?.slice(30, 60);
     //   }
-      items = newVideos?.map(function (item) {
-        return {
-          id: item?.id,
-          title: item?.title,
-          duration: item?.duration,
-          description: item?.description,
-          isLive: item?.isLive,
-          viewCount: item?.viewCount,
-          uploadDate: item?.uploadDate,
-          thumbnail: `https://img.youtube.com/vi/${item?.id}/mqdefault.jpg`,
-          channelName: item?.channel.name,
-          channelID: item?.channel.id,
-        };
-      });
+      
       res.status(200).json(items);
     } else {
 
