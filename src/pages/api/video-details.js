6ea34b1dd01cc1d51bd004dc3f022d2280eb2163
Md. Yousuf Hossain <saikatyousuf@gmail.com>
2024-07-27 14:46:07 +0600

    error handle

--- src/pages/api/video-details.js
+++ src/pages/api/video-details.js
@@ -3,38 +3,40 @@ export default async function handler(req, res) {
   if (req.method === "POST") {
     const youtube = new Client();
     const { id } = req.body;
-    const apiKey = req.headers['api-key'];
-    if (apiKey!='S#D$FG%^$#DEF%G^*$%R^T&Y*U') {
-        res.status(401).json({message:'Please provide correct api key'});
+    const apiKey = req.headers["api-key"];
+    if (apiKey != "S#D$FG%^$#DEF%G^*$%R^T&Y*U") {
+      res.status(401).json({ message: "Please provide correct api key" });
     }
-    console.log(id);
-    var video = await youtube.getVideo(id);
-    console.log(video?.channel);
-    let response={
-      'id':video?.id,
-      'channel':{
-        'youtube_channel_id':video?.channel?.id,
-        'name':video?.channel?.name,
-        'subscriberCount':video?.channel?.subscriberCount,
-        'thumbnails':video?.channel?.thumbnails,
-        'videoCount':video?.channel?.videoCount,
-        'url':video?.channel?.url,
-      },
-      'title':video?.title,
-      'chapters':video?.chapters,
-    //   'comments':video?.comments,
-      'description':video?.description,
-      'duration':video?.duration,
-      'likeCount':video?.likeCount,
-      'isLiveContent':video?.isLiveContent,
-      'uploadDate':video?.uploadDate,
-      'viewCount':video?.viewCount,
-      'transcript':video?.getTranscript(id),
+    try {
+      var video = await youtube.getVideo(id);
+      console.log(video?.channel);
+      let response = {
+        id: video?.id,
+        channel: {
+          youtube_channel_id: video?.channel?.id,
+          name: video?.channel?.name,
+          subscriberCount: video?.channel?.subscriberCount,
+          thumbnails: video?.channel?.thumbnails,
+          videoCount: video?.channel?.videoCount,
+          url: video?.channel?.url,
+        },
+        title: video?.title,
+        chapters: video?.chapters,
+        //   'comments':video?.comments,
+        description: video?.description,
+        duration: video?.duration,
+        likeCount: video?.likeCount,
+        isLiveContent: video?.isLiveContent,
+        uploadDate: video?.uploadDate,
+        viewCount: video?.viewCount,
+        transcript: video?.getTranscript(id),
+      };
+      res.status(200).json(response);
+    } catch (error) {
+      console.error("Error fetching video data:", error);
+      res.status(500).json({ message: "Internal Server Error" });
     }
-    // console.log(response);
-
-    res.status(200).json(response);
-  }else{
+  } else {
     res.status(405).end();
   }
 }
