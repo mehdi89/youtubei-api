35eb0ea6b08896020cb8d233287df79207bd5065
Md. Golam Shahaj Uddin <gshahaj@gmail.com>
2024-10-29 11:55:32 +0600

    Transcript: add language param

--- src/pages/api/transcript.js
+++ src/pages/api/transcript.js
@@ -1,53 +1,58 @@
-import { Client } from "youtubei";
-import { YoutubeTranscript } from "youtube-transcript";
+import { Client } from 'youtubei';
+import { YoutubeTranscript } from 'youtube-transcript';
 
 function decodeEntities(encodedString) {
   var translate_re = /&(nbsp|amp|quot|lt|gt);/g;
   var translate = {
-      "nbsp":" ",
-      "amp" : "&",
-      "quot": "\"",
-      "lt"  : "<",
-      "gt"  : ">"
+    nbsp: ' ',
+    amp: '&',
+    quot: '"',
+    lt: '<',
+    gt: '>',
   };
-  return encodedString.replace(translate_re, function(match, entity) {
+  return encodedString
+    .replace(translate_re, function (match, entity) {
       return translate[entity];
-  }).replace(/&#(\d+);/gi, function(match, numStr) {
+    })
+    .replace(/&#(\d+);/gi, function (match, numStr) {
       var num = parseInt(numStr, 10);
       return String.fromCharCode(num);
-  });
+    });
 }
 
 export default async function handler(req, res) {
-  if (req.method === "POST") {
+  if (req.method === 'POST') {
     const youtube = new Client();
-    const { id, type } = req.body;
-    const apiKey = req.headers["api-key"];
-    if (apiKey != "S#D$FG%^$#DEF%G^*$%R^T&Y*U") {
-      res.status(401).json({ message: "Please provide correct api key" });
+    const { id, type, lang } = req.body;
+    const apiKey = req.headers['api-key'];
+    if (apiKey != 'S#D$FG%^$#DEF%G^*$%R^T&Y*U') {
+      res.status(401).json({ message: 'Please provide correct api key' });
     }
 
+    const options = lang ? { lang:lang } : {};
+
     try {
-      const transcript = await YoutubeTranscript.fetchTranscript(id);
-      var data = "";
-      if (type == "timestamped") {
+      const transcript = await YoutubeTranscript.fetchTranscript(id, options);
+
+      var data = '';
+      if (type == 'timestamped') {
         transcript.forEach((entry) => {
           var timedString =
-            "time : " + entry.offset + " second. Text: " + entry.text;
+            'time : ' + entry.offset + ' second. Text: ' + entry.text;
           data += timedString;
         });
       } else {
         transcript.forEach((entry) => {
-          data += " " + entry.text;
+          data += ' ' + entry.text;
         });
       }
       data = decodeEntities(data);
-      
+
       res.status(200).json({
         data: data,
       });
     } catch (error) {
-      console.error("Error fetching video data:", error);
+      console.error('Error fetching video data:', error);
       res.status(500).json({ message: error.message });
     }
   } else {
